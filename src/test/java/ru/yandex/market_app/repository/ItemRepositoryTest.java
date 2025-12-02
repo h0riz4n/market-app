e1e0f77932100b0652c0ed508aca90b70c4d995e
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2025-12-02 23:40:40 +0300

    test: Переписаны интеграционные тесты с базой данных на реактивный стиль

--- src/test/java/ru/yandex/market_app/repository/ItemRepositoryTest.java
+++ src/test/java/ru/yandex/market_app/repository/ItemRepositoryTest.java
@@ -1,33 +1,37 @@
 package ru.yandex.market_app.repository;
 
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import ru.yandex.market_app.container.DatabaseContainer;
-import ru.yandex.market_app.model.entity.ItemEntity;
-import ru.yandex.market_app.model.filter.ItemFilterModel;
-import ru.yandex.market_app.repository.specification.ItemSpecification;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import ru.yandex.market_app.container.DatabaseContainerTest;
+import ru.yandex.market_app.model.domain.Item;
+import ru.yandex.market_app.model.filter.ItemFilterModel;
+import ru.yandex.market_app.repository.impl.ItemRepositoryImpl;
+import ru.yandex.market_app.repository.specification.ItemSpecification;
 
 @Tag("integration")
-@DataJpaTest
+@DataR2dbcTest
 @Testcontainers
-@ImportTestcontainers(DatabaseContainer.class)
+@Import(ItemRepositoryImpl.class)
+@ImportTestcontainers(DatabaseContainerTest.class)
 public class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepo;
 
-    private ItemEntity mockItem;
+    private Item mockItem;
 
     @BeforeEach
     public void beforeEach() {
@@ -36,36 +40,40 @@ public class ItemRepositoryTest {
 
     @AfterEach
     public void afterEach() {
-        itemRepo.deleteAll();
+        itemRepo.deleteAll().block();
     }
 
     @Test
     public void testFindByTitle() {
-        var item = itemRepo.findByTitle(mockItem.getTitle());
-        assertTrue(item.isPresent());
-        assertEquals(mockItem, item.get());
+        itemRepo.findByTitle(mockItem.getTitle()).subscribe(item -> {
+            assertEquals(mockItem, item);
+        });
     }
 
     @Test
     public void testFindAllByCartCountGreaterThan() {
         mockItem.setCartCount(1);
-        itemRepo.save(mockItem);
+        itemRepo.updateCartCount(mockItem).block();
 
-        var items = itemRepo.findAllByCartCountGreaterThan(0);
 
-        assertTrue(items.contains(mockItem));
+        itemRepo.findAllByCartCountGreaterThan(0)
+            .collectList()
+            .subscribe(items -> {
+                assertTrue(items.contains(mockItem));
+            });
     }
 
     @Test
-    @Transactional
     public void testUpdateAll() {
         mockItem.setCartCount(1);
-        itemRepo.save(mockItem);
+        itemRepo.updateCartCount(mockItem).block();
+        itemRepo.upadteAll().block();
 
-        itemRepo.upadteAll();
-
-        var items = itemRepo.findAllByCartCountGreaterThan(0);
-        assertFalse(items.contains(mockItem));
+        itemRepo.findAllByCartCountGreaterThan(0)
+            .collectList()
+            .subscribe(items -> {
+                assertFalse(items.contains(mockItem));
+            });
     }
 
     @Test
@@ -74,17 +82,20 @@ public class ItemRepositoryTest {
             .search(mockItem.getTitle().substring(0, 3))
             .build();
 
-        var items = itemRepo.findAll(new ItemSpecification(filter));
-        assertTrue(items.contains(mockItem));
+        var specification = new ItemSpecification();
+        itemRepo.findAll(specification.toCriteria(filter), PageRequest.of(0, 5)).subscribe(page -> {
+            assertTrue(page.getContent().contains(mockItem));
+        });
     }
 
-    private ItemEntity createItem() {
-        var item = ItemEntity.builder()
+
+    private Item createItem() {
+        var item = Item.builder()
             .title("Футбольный мяч")
             .description("Большой футбольный мяч для игры на улице")
             .price(100)
             .image("image/1.jpg")
             .build();
-        return itemRepo.save(item);
+        return itemRepo.save(item).block();
     }
 }

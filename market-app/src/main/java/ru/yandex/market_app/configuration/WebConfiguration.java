fab2fc2780a6bb88d59b319acd7b77d164595949
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2025-12-19 01:18:25 +0300

    fix: Небольшие изменения при сборке

--- market-app/src/main/java/ru/yandex/market_app/configuration/WebConfiguration.java
+++ market-app/src/main/java/ru/yandex/market_app/configuration/WebConfiguration.java
@@ -1,9 +1,11 @@
 package ru.yandex.market_app.configuration;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.RequiredArgsConstructor;
 import ru.yandex.market_app.property.MarketAppProperty;
@@ -21,5 +23,14 @@ public class WebConfiguration implements WebFluxConfigurer {
             .allowedOrigins(property.getCorsOrigins()) 
             .allowedMethods("*")
             .allowedHeaders("*");    
+    }
+
+    @Bean
+    public PaymentApi paymentApi(WebClient.Builder builder) {
+        // Указываем базовый URL API
+        ApiClient apiClient = new ApiClient(
+            builder.baseUrl("http://localhost:8080").build()
+        );
+        return new PaymentApi(apiClient);
     }
 }

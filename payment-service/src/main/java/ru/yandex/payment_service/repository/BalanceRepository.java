f92a069b4a6aedde20cb99c6ca34fcc46d06bd0d
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2026-01-19 15:58:33 +0300

    fix: Изменены сущности и репозитории

--- payment-service/src/main/java/ru/yandex/payment_service/repository/BalanceRepository.java
+++ payment-service/src/main/java/ru/yandex/payment_service/repository/BalanceRepository.java
@@ -1,11 +1,15 @@
 package ru.yandex.payment_service.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
+import reactor.core.publisher.Mono;
 import ru.yandex.payment_service.model.domain.Balance;
 
 @Repository
-public interface BalanceRepository extends  R2dbcRepository<Balance, Long>  {
+public interface BalanceRepository extends R2dbcRepository<Balance, Long> {
 
+    Mono<Balance> findByUserId(UUID userId);
 }
